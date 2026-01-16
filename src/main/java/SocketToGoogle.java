761a636adae1b95b10380c3e27cc8d793c8ed40f
tewaris <statsusant@gmail.com>
2026-01-16 17:34:49 +0530

    SocketToGoogle

---
+++ src/main/java/SocketToGoogle.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SocketToGoogle {
+
+  public static void main(String[] args) {
+
+    try {
+
+      Socket socket = new Socket("www.google.com", 80);
+
+      OutputStream os = socket.getOutputStream();
+
+      os.write(("GET / HTTP/1.1\r\n"
+          + "Host: www.google.com\r\n\r\n").getBytes());
+      os.flush();
+
+      BufferedReader reader = new BufferedReader(
+          new InputStreamReader(socket.getInputStream())
+      );
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
