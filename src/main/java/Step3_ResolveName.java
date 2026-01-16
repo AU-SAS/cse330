7fcbaaccab6a9bc9eb4eeda5c627d87cb15dc85c
tewaris <statsusant@gmail.com>
2026-01-16 19:04:08 +0530

    added Step3_ResolveName

---
+++ src/main/java/Step3_ResolveName.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Step3_ResolveName {
+
+  public static void main(String[] args) {
+
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    input.lines().forEach((line) -> {
+
+      try {
+
+        InetAddress address = InetAddress.getByName(line);
+
+        System.out.println(address.getHostAddress());
+
+      } catch (UnknownHostException e) {
+        System.out.println("Unknown host");
+      }
+
+    });
+  }
+
+}
